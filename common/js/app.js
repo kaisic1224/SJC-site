7e88224dbaa11da49e95bece15833f2d87ad0582
kaisic1224 <89894485+kaisic1224@users.noreply.github.com>
2021-12-28 02:09:17 -0500

    Add files via upload

--- common/js/app.js
+++ common/js/app.js
@@ -3,10 +3,19 @@
 (function (global) {
 
     const resources = document.querySelector('.resources');
+    // MAYBE DATABASE ???
 
+    const menu = document.querySelector('.menu-btn');
+    const openMenu = document.querySelector('.menu-btn-expanded')
 
+    menu.addEventListener('click', () => {
+        document.querySelector('.mbl-menu').classList.toggle('active');
+        document.querySelector('.mbl-menu').classList.toggle('collapse');
+    });
 
-    resources.appendChild
-
+    openMenu.addEventListener('click', () => {
+        document.querySelector('.mbl-menu').classList.toggle('collapse');
+        document.querySelector('.mbl-menu').classList.toggle('active');
+    })
 
 }(typeof window != "undefined" ? window : this))

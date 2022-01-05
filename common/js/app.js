300270de2538620c5d7546f736c129ad58d56c56
kaisic1224 <89894485+kaisic1224@users.noreply.github.com>
2022-01-04 23:11:01 -0500

    Add files via upload

--- common/js/app.js
+++ common/js/app.js
@@ -8,6 +8,7 @@
     const menu = document.querySelector('.menu-btn');
     const openMenu = document.querySelector('.menu-btn-expanded');
     const mblMenu = document.querySelector('.mbl-menu');
+    const fadeIn = document.querySelectorAll('.fade-in-up');
     const links = document.querySelectorAll('.nav-link');
 
 
@@ -20,6 +21,25 @@
     openMenu.addEventListener('click', () => {
         mblMenu.classList.toggle('collapse');
         mblMenu.classList.toggle('active');
+    });
+
+
+
+
+    const appearOptions = {
+        threshold: 0.25,
+        rootMargin: '0px 0px -50px 0px'
+    }
+    const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) return;
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        });
+    }, appearOptions);
+
+    fadeIn.forEach(fade => {
+        appearOnScroll.observe(fade);
     });
 
 }(typeof window != "undefined" ? window : this))
